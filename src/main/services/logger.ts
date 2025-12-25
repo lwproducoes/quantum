1147653e4bb709c22a15e342d301e5019537ec45
João Guilherme <j4g3dev@gmail.com>
2025-12-25 18:34:26 -0300

    feat: implement custom log file paths based on message scope and level (#7)
    * feat: implement custom log file paths based on message scope and level
    * chore: bump version to 1.0.5

--- src/main/services/logger.ts
+++ src/main/services/logger.ts
@@ -1,4 +1,35 @@
+import { app } from 'electron'
 import log from 'electron-log'
+import path from 'path'
+
+const logsPath = path.join(app.getPath('userData'), `logs${app.isPackaged ? '' : '-dev'}`)
+console.log(`Logs path: ${logsPath}`)
+log.transports.file.resolvePathFn = (
+  _: log.PathVariables,
+  message?: log.LogMessage | undefined
+) => {
+  if (message?.scope === 'python-rpc') {
+    return path.join(logsPath, 'pythonrpc.txt')
+  }
+
+  if (message?.scope === 'network') {
+    return path.join(logsPath, 'network.txt')
+  }
+
+  if (message?.scope == 'achievements') {
+    return path.join(logsPath, 'achievements.txt')
+  }
+
+  if (message?.level === 'error') {
+    return path.join(logsPath, 'error.txt')
+  }
+
+  if (message?.level === 'info') {
+    return path.join(logsPath, 'info.txt')
+  }
+
+  return path.join(logsPath, 'logs.txt')
+}
 
 log.errorHandler.startCatching({
   showDialog: false
